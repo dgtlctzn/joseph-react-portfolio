fd2e8e7a9bf61b00dac1a9b1cece2b6eb3e3ca77
Joseph Perry <josephperry720@gmail.com>
2020-11-02 19:47:51 -0500

    added test, get routes and middleware

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("client/build"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true
+    })
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`running on http://localhost:${PORT}`)
+})
