f47a88455d4f4841145818c8172dc078860fa9cd
Joseph Perry <josephperry720@gmail.com>
2020-11-03 16:53:31 -0500

    added cards with project information

--- client/src/components/ProjectCard/ProjectCard.jsx
+++ client/src/components/ProjectCard/ProjectCard.jsx
@@ -1,30 +1,33 @@
 import React, { PureComponent } from "react";
 import { Card, CardTitle, Icon } from "react-materialize";
 import { Link } from "react-router-dom";
+import "./ProjectCard.css";
 
-const ProjectCard = (props) => {
+const ProjectCard = ({ name, link, gh_link, image, icon, text, tech }) => {
   return (
     <div className="col s12 m4">
       <Card
         closeIcon={<Icon>close</Icon>}
-        header={
-          <CardTitle
-            image="https://materializecss.com/images/sample-1.jpg"
-            reveal
-            waves="light"
-          />
-        }
+        header={<CardTitle image={image} reveal waves="light" />}
         reveal={
-          <p>
-            Here is some more information about this product that is only
-            revealed once clicked on.
-          </p>
+          <>
+            <p>{text}</p>
+            <i className={icon}></i>
+            <ul>
+              {tech.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+            <p>
+              <a href={gh_link}>GitHub</a>
+            </p>
+          </>
         }
-        revealIcon={<Icon>more_vert</Icon>}
-        title={props.name}
+        revealIcon={<Icon>more...</Icon>}
+        title={link.length > 0 ? <a href={link}>{name}</a> : name}
       >
         <p>
-          <Link href="/">This is a link</Link>
+          <a href={gh_link}>GitHub</a>
         </p>
       </Card>
     </div>
--- client/src/containers/Projects/Projects.jsx
+++ client/src/containers/Projects/Projects.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Row, Col } from "react-materialize";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import projects from "../../json/projects.json";
 
 const Projects = () => {
-  const projects = ["one", "two", "three", "four"];
-
   return (
     <div className="container">
       <h1>Project page</h1>
       <Row>
-        {projects.map((project) => (
-          <ProjectCard name={project} />
+        {/* maps over json array of objects*/}
+        {projects.map(({ id, name, link, gh_link, image, icon, text, tech }) => (
+          <ProjectCard
+            key={id}
+            name={name}
+            link={link}
+            gh_link={gh_link}
+            image={image}
+            icon={icon}
+            text={text}
+            tech={tech}
+          />
         ))}
       </Row>
     </div>
